3e664b4b935718a89fb7fb6fa651980b1b5104b5
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2021-12-24 00:44:11 +0900

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,28 @@ int main(int argc, char* argv[])
   noecho(); // hide key input
 
   curs_set(0); // hide cursor
-  
-  mvprintw(12, 30, "hello world!");
 
+  keypad(stdscr, TRUE);
+
+  mousemask(ALL_MOUSE_EVENTS, NULL);
+  MEVENT e;
+  int px = 2;
+  
   while (true)
   {
     int ch = getch();
-    if (ch == 'q')
-    {
+    if (ch == 'q') {
       break;
+    }
+    if (ch == KEY_MOUSE) {
+      if (getmouse(&e) == OK) {
+        clear();
+        px = e.x;
+        if (px < 2) { px = 2; }
+        if (px > 77) { px = 77; }
+        mvprintw(23, px -2, "-----");
+        refresh();
+      }
     }
   }
   
