2e86603a8d0e55dab66fbc3eec60c0c2abcb5b33
Yuji Iwanaga <iwanaga@azlab.co.jp>
2023-01-30 17:50:41 +0900

    update test/01, vectorとsleepを使って一行ずつ出すようにしてみた

--- test/01_output_aa/main/main.cpp
+++ test/01_output_aa/main/main.cpp
@@ -1,5 +1,9 @@
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <thread>
 
 int main(int argc, char *argv[])
 {
@@ -13,13 +17,22 @@ int main(int argc, char *argv[])
     ######## ##     ## ##     ##  ######   ######
    */
 
-  std::cout << " ######## ##     ##    ###     ######   ######  " << std::endl;
-  std::cout << " ##       ###   ###   ## ##   ##    ## ##    ## " << std::endl;
-  std::cout << " ##       #### ####  ##   ##  ##       ##       " << std::endl;
-  std::cout << " ######   ## ### ## ##     ## ##        ######  " << std::endl;
-  std::cout << " ##       ##     ## ######### ##             ## " << std::endl;
-  std::cout << " ##       ##     ## ##     ## ##    ## ##    ## " << std::endl;
-  std::cout << " ######## ##     ## ##     ##  ######   ######  " << std::endl;
+  // init
+  std::vector<std::string> vecStr;
+  vecStr.push_back(" ######## ##     ##    ###     ######   ######  ");
+  vecStr.push_back(" ##       ###   ###   ## ##   ##    ## ##    ## ");
+  vecStr.push_back(" ##       #### ####  ##   ##  ##       ##       ");
+  vecStr.push_back(" ######   ## ### ## ##     ## ##        ######  ");
+  vecStr.push_back(" ##       ##     ## ######### ##             ## ");
+  vecStr.push_back(" ##       ##     ## ##     ## ##    ## ##    ## ");
+  vecStr.push_back(" ######## ##     ## ##     ##  ######   ######  ");
+
+  // output
+  for (int i = 0; i < vecStr.size(); i++)
+  {
+    std::cout << vecStr[i] << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
 
   return 0;
 }
