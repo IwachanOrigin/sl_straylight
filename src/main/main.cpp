d6424e9ed31a12e93a9682199db6b6b7fba783b3
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2023-07-19 01:49:04 +0900

    src, scroll start pos to right

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -30,7 +30,7 @@ static inline void init_curses()
   scrollok(stdscr, false);
 }
 
-static inline void mv_str(int& y, int& x, const std::string& str)
+static inline void mv_str(int y, int x, const std::string& str)
 {
   int strPos = 0;
   for (; x < 0; ++x, strPos++)
@@ -62,6 +62,29 @@ int main(int argc, char *argv[])
     erase();
     x_pos--;
 
+    mv_str(y - 10, x_pos, SL_LOGO_LINE_001);
+    mv_str(y - 9, x_pos, SL_LOGO_LINE_002);
+    mv_str(y - 8, x_pos, SL_LOGO_LINE_003);
+    mv_str(y - 7, x_pos, SL_LOGO_LINE_004);
+    mv_str(y - 6, x_pos, SL_LOGO_LINE_005);
+    mv_str(y - 5, x_pos, SL_LOGO_LINE_006);
+    mv_str(y - 4, x_pos, SL_LOGO_LINE_007);
+    mv_str(y - 3, x_pos, SL_LOGO_LINE_008);
+    mv_str(y - 2, x_pos, SL_LOGO_LINE_009);
+    mv_str(y - 1, x_pos, SL_LOGO_LINE_010);
+    mv_str(y + 0, x_pos, SL_LOGO_LINE_011);
+    mv_str(y + 1, x_pos, SL_LOGO_LINE_012);
+    mv_str(y + 2, x_pos, SL_LOGO_LINE_013);
+    mv_str(y + 3, x_pos, SL_LOGO_LINE_014);
+    mv_str(y + 4, x_pos, SL_LOGO_LINE_015);
+    mv_str(y + 5, x_pos, SL_LOGO_LINE_016);
+    mv_str(y + 6, x_pos, SL_LOGO_LINE_017);
+    mv_str(y + 7, x_pos, SL_LOGO_LINE_018);
+    mv_str(y + 8, x_pos, SL_LOGO_LINE_019);
+    mv_str(y + 9, x_pos, SL_LOGO_LINE_020);
+    mv_str(y + 10, x_pos, SL_LOGO_LINE_021);
+
+#if 0
     mvwprintw(stdscr, y - 10, x_pos, "%s", SL_LOGO_LINE_001.c_str());
     mvwprintw(stdscr, y - 9, x_pos, "%s", SL_LOGO_LINE_002.c_str());
     mvwprintw(stdscr, y - 8, x_pos, "%s", SL_LOGO_LINE_003.c_str());
@@ -83,7 +106,7 @@ int main(int argc, char *argv[])
     mvwprintw(stdscr, y + 8, x_pos, "%s", SL_LOGO_LINE_019.c_str());
     mvwprintw(stdscr, y + 9, x_pos, "%s", SL_LOGO_LINE_020.c_str());
     mvwprintw(stdscr, y + 10, x_pos, "%s", SL_LOGO_LINE_021.c_str());
-
+#endif
     refresh();
 
     usleep(40000);
