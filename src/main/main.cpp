7f68e9a6fe6234e12996f31ce97a2acfe60cec61
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2023-07-19 01:01:04 +0900

    src, update

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -20,52 +20,41 @@
 #define CAT2 "( o.o ) "
 #define CAT3 " > ^ < "
 
-void init_curses()
+static inline void init_curses()
 {
   initscr();
-  start_color();
-  init_pair(1,COLOR_WHITE,COLOR_BLUE);
-  init_pair(2,COLOR_BLUE,COLOR_WHITE);
-  init_pair(3,COLOR_RED,COLOR_WHITE);
   curs_set(0);
   noecho();
-  keypad(stdscr,TRUE);
+  nodelay(stdscr, true);
+  leaveok(stdscr, true);
+  scrollok(stdscr, false);
+  keypad(stdscr, true);
 }
 
 int main(int argc, char *argv[])
 {
   int key;
-  WINDOW *messagebar = nullptr;
-  WINDOW *scrollmessage = nullptr;
-  int width = 0;
-  int height = 0;
 
   // init
   init_curses();
 
-  // clear background color.
-  bkgd(COLOR_PAIR(1));
-
-  // Get max size of height, width
-  getmaxyx(stdscr, height, width);
   // Set max width
-  int x_pos = width;
+  int x_pos = COLS;
 
-  // subwin(WINDOW*, int nlines, int ncols, int begin_y, int begin_x)
-  printw("ESC quits.");
   refresh();
 
   do
   {
     // clear
     erase();
+    x_pos--;
+    
     mvwprintw(stdscr, 2, x_pos, "%s", SL_LOGO_LINE_001.c_str());
     mvwprintw(stdscr, 3, x_pos, "%s", SL_LOGO_LINE_002.c_str());
     mvwprintw(stdscr, 4, x_pos, "%s", SL_LOGO_LINE_003.c_str());
     mvwprintw(stdscr, 5, x_pos, "%s", SL_LOGO_LINE_004.c_str());
     mvwprintw(stdscr, 6, x_pos, "%s", SL_LOGO_LINE_005.c_str());
-    mvaddstr(8, x_pos, "Hello World");
-    x_pos--;
+
     // The touchwin func raises a flag in the WINDOW structure to inform the refresh func that all lines have changed.
     touchwin(stdscr);
     refresh();
