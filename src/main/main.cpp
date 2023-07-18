c82ea2580d6518277ea1fc67de7820bbcf314059
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2023-07-19 01:15:33 +0900

    src, rendering to center

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -33,14 +33,12 @@ static inline void init_curses()
 
 int main(int argc, char *argv[])
 {
-  int key;
-
   // init
   init_curses();
 
   // Set max width
-  int x_pos = COLS;
-
+  int x_pos = COLS - 1;
+  int y = LINES / 2;
   refresh();
 
   do
@@ -49,27 +47,27 @@ int main(int argc, char *argv[])
     erase();
     x_pos--;
     
-    mvwprintw(stdscr, 2, x_pos, "%s", SL_LOGO_LINE_001.c_str());
-    mvwprintw(stdscr, 3, x_pos, "%s", SL_LOGO_LINE_002.c_str());
-    mvwprintw(stdscr, 4, x_pos, "%s", SL_LOGO_LINE_003.c_str());
-    mvwprintw(stdscr, 5, x_pos, "%s", SL_LOGO_LINE_004.c_str());
-    mvwprintw(stdscr, 6, x_pos, "%s", SL_LOGO_LINE_005.c_str());
-    mvwprintw(stdscr, 7, x_pos, "%s", SL_LOGO_LINE_006.c_str());
-    mvwprintw(stdscr, 8, x_pos, "%s", SL_LOGO_LINE_007.c_str());
-    mvwprintw(stdscr, 9, x_pos, "%s", SL_LOGO_LINE_008.c_str());
-    mvwprintw(stdscr, 10, x_pos, "%s", SL_LOGO_LINE_009.c_str());
-    mvwprintw(stdscr, 11, x_pos, "%s", SL_LOGO_LINE_010.c_str());
-    mvwprintw(stdscr, 12, x_pos, "%s", SL_LOGO_LINE_011.c_str());
-    mvwprintw(stdscr, 13, x_pos, "%s", SL_LOGO_LINE_012.c_str());
-    mvwprintw(stdscr, 14, x_pos, "%s", SL_LOGO_LINE_013.c_str());
-    mvwprintw(stdscr, 15, x_pos, "%s", SL_LOGO_LINE_014.c_str());
-    mvwprintw(stdscr, 16, x_pos, "%s", SL_LOGO_LINE_015.c_str());
-    mvwprintw(stdscr, 17, x_pos, "%s", SL_LOGO_LINE_016.c_str());
-    mvwprintw(stdscr, 18, x_pos, "%s", SL_LOGO_LINE_017.c_str());
-    mvwprintw(stdscr, 19, x_pos, "%s", SL_LOGO_LINE_018.c_str());
-    mvwprintw(stdscr, 20, x_pos, "%s", SL_LOGO_LINE_019.c_str());
-    mvwprintw(stdscr, 21, x_pos, "%s", SL_LOGO_LINE_020.c_str());
-    mvwprintw(stdscr, 22, x_pos, "%s", SL_LOGO_LINE_021.c_str());
+    mvwprintw(stdscr, y - 10, x_pos, "%s", SL_LOGO_LINE_001.c_str());
+    mvwprintw(stdscr, y - 9, x_pos, "%s", SL_LOGO_LINE_002.c_str());
+    mvwprintw(stdscr, y - 8, x_pos, "%s", SL_LOGO_LINE_003.c_str());
+    mvwprintw(stdscr, y - 7, x_pos, "%s", SL_LOGO_LINE_004.c_str());
+    mvwprintw(stdscr, y - 6, x_pos, "%s", SL_LOGO_LINE_005.c_str());
+    mvwprintw(stdscr, y - 5, x_pos, "%s", SL_LOGO_LINE_006.c_str());
+    mvwprintw(stdscr, y - 4, x_pos, "%s", SL_LOGO_LINE_007.c_str());
+    mvwprintw(stdscr, y - 3, x_pos, "%s", SL_LOGO_LINE_008.c_str());
+    mvwprintw(stdscr, y - 2, x_pos, "%s", SL_LOGO_LINE_009.c_str());
+    mvwprintw(stdscr, y - 1, x_pos, "%s", SL_LOGO_LINE_010.c_str());
+    mvwprintw(stdscr, y + 0, x_pos, "%s", SL_LOGO_LINE_011.c_str());
+    mvwprintw(stdscr, y + 1, x_pos, "%s", SL_LOGO_LINE_012.c_str());
+    mvwprintw(stdscr, y + 2, x_pos, "%s", SL_LOGO_LINE_013.c_str());
+    mvwprintw(stdscr, y + 3, x_pos, "%s", SL_LOGO_LINE_014.c_str());
+    mvwprintw(stdscr, y + 4, x_pos, "%s", SL_LOGO_LINE_015.c_str());
+    mvwprintw(stdscr, y + 5, x_pos, "%s", SL_LOGO_LINE_016.c_str());
+    mvwprintw(stdscr, y + 6, x_pos, "%s", SL_LOGO_LINE_017.c_str());
+    mvwprintw(stdscr, y + 7, x_pos, "%s", SL_LOGO_LINE_018.c_str());
+    mvwprintw(stdscr, y + 8, x_pos, "%s", SL_LOGO_LINE_019.c_str());
+    mvwprintw(stdscr, y + 9, x_pos, "%s", SL_LOGO_LINE_020.c_str());
+    mvwprintw(stdscr, y + 10, x_pos, "%s", SL_LOGO_LINE_021.c_str());
 
     // The touchwin func raises a flag in the WINDOW structure to inform the refresh func that all lines have changed.
     touchwin(stdscr);
@@ -78,7 +76,7 @@ int main(int argc, char *argv[])
     usleep(50000);
 
   }
-  while (key != ESC && x_pos > 0);
+  while (x_pos > 0);
 
   endwin();
   return 0;
